eb2c9a5e78667d2925050cec725f815004782c32
wottan32 <mariotorreslagos@gmail.com>
2023-07-30 19:37:43 -0400

    new dev

--- src/main/java/com/bolsaideas/springbootweb/controllers/IndexController.java
+++ src/main/java/com/bolsaideas/springbootweb/controllers/IndexController.java
@@ -1,10 +1,13 @@
 package com.bolsaideas.springbootweb.controllers;
 
+import com.bolsaideas.springbootweb.web.app.models.Usuario;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
 
     @GetMapping(value = {"/index", "/", "/home"})
@@ -12,5 +15,23 @@ public class IndexController {
         model.addAttribute("titulo",
                 "Mario is the Best!");
     return "index";
+    }
+    @RequestMapping("/perfil")
+    public String perfil(Model model) {
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Mario");
+        usuario.setApellido("Torres");
+        usuario.setEmail("mariotorreslagos@gmail.com");
+
+        model.addAttribute("usuario", usuario);
+        model.addAttribute("titulo", "Perfil del usuario: ".concat(usuario.getNombre()));
+
+        return "perfil";
+    }
+
+    @RequestMapping("/listar")
+    public String listar(Model model) {
+        model.addAttribute("titulo", "Listado de usuarios");
+        return "listar";
     }
 }
